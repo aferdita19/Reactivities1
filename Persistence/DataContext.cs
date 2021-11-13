69b595201ba3d2e586219bcdf51b5394ac96774d
aferdita19 <ag34280@ubt-uni.net>
2021-11-13 09:09:01 +0100

    End of section 12

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
 
         }
 
-        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Activity> Activities { get; set; } 
     }
 }
