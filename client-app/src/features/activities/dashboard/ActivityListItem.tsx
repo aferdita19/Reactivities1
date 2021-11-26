72083f35f1b50fda0db279c3a208b29675641f6e
aferdita19 <ag34280@ubt-uni.net>
2021-11-26 14:03:35 +0100

    End of section 17

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -18,12 +18,12 @@ export default observer(function  ActivityListItem({activity}: Props){
                }
                <Item.Group>
                    <Item>
-                       <Item.Image style={{marginBottom : 3}} size='tiny' circular src='/assets/user.png'/>
+                       <Item.Image style={{marginBottom : 3}} size='tiny' circular src={activity.host?.image || '/assets/user.png'}/>
                        <Item.Content>
                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                            </Item.Header>
-                           <ItemDescription>Hosted By{activity.host?.displayName}</ItemDescription>
+                           <ItemDescription>Hosted By<Link to={`/profiles/${activity.hostUsername}`}>{activity.host?.displayName}</Link></ItemDescription>
                            {activity.isHost && (
                                <Item.Description >
                                    <Label basic color='orange'>You are hosting this activity </Label>
